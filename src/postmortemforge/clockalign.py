7b487653e84d84e6695badfeb88efc0c9b4a7303
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2015-09-03 15:46:00 +0100

    style: clean up python

--- src/postmortemforge/clockalign.py
+++ src/postmortemforge/clockalign.py
@@ -11,3 +11,8 @@ each source clock with a linear map:
   slow). skew multiplies the elapsed time since a per-source anchor, so a small
   rate error accumulates over the window rather than applying uniformly.
 - anchor is the raw_ts at which the source was last known to agree with the
+  reference. Using an anchor keeps the correction numerically small and makes
+  the offset the pure shift at the anchor instant.
+
+The maps are declared, not inferred: this tool aligns against offsets an
+operator provides (from NTP records, a known deploy marker, and so on). The
