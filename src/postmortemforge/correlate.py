b7dc945e588576e10ade36bcc0e09b7922af0975
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2015-11-26 11:34:00 +0000

    build: reports

--- src/postmortemforge/correlate.py
+++ src/postmortemforge/correlate.py
@@ -29,3 +29,11 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from .clockalign import AlignedEvent
+
+
+@dataclass(frozen=True)
+class Interval:
+    """A closed time interval on the reference clock, with its bounding events."""
+
+    start_ts: float
