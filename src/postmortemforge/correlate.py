a784d96ed98b476f7b56fcedf78e3769c80c6a4a
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2015-12-30 16:27:00 +0000

    build: drafting

--- src/postmortemforge/correlate.py
+++ src/postmortemforge/correlate.py
@@ -76,3 +76,10 @@ def deploy_events(events: list[AlignedEvent]) -> list[AlignedEvent]:
 
 
 def breach_intervals(events: list[AlignedEvent], break_gap_s: float = 120.0) -> list[Interval]:
+    """Find metric breach intervals from breached metric samples.
+
+    Consecutive breached samples belong to the same interval unless separated by
+    more than break_gap_s, which starts a new one.
+    """
+    breached = [e for e in events if e.source == "metric" and e.event.attrs.get("breached")]
+    breached.sort(key=lambda e: e.ref_ts)
