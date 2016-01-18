94bdc0cfdcdd762ad6bcc4f760e580656ddd3dae
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-01-18 16:41:00 +0000

    test: cover ci

--- src/postmortemforge/timeline.py
+++ src/postmortemforge/timeline.py
@@ -7,3 +7,7 @@ renderer read from, so the incident is described once and consumed many ways.
 Time is presented as minutes elapsed from the first event, which is the natural
 axis for a postmortem and keeps the diagram and the prose using the same units.
 """
+
+from __future__ import annotations
+
+import datetime as _dt
