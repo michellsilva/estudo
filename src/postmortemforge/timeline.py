752cb93ac85a672c12f0e339321ca3cb6f2e6bd3
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-01-18 16:11:00 +0000

    feat: postmortem

---
+++ src/postmortemforge/timeline.py
@@ -0,0 +1,6 @@
+"""The ordered timeline model.
+
+A Timeline is the aligned, merged event stream plus the correlation features and
+links found over it. It is the single structure the draft writer and the report
+renderer read from, so the incident is described once and consumed many ways.
+
