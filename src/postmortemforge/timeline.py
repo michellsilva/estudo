30ce632a2908c1cd253389f89d9e4b287fd398ad
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-01-18 16:23:00 +0000

    feat: add incident

--- src/postmortemforge/timeline.py
+++ src/postmortemforge/timeline.py
@@ -4,3 +4,6 @@ A Timeline is the aligned, merged event stream plus the correlation features and
 links found over it. It is the single structure the draft writer and the report
 renderer read from, so the incident is described once and consumed many ways.
 
+Time is presented as minutes elapsed from the first event, which is the natural
+axis for a postmortem and keeps the diagram and the prose using the same units.
+"""
