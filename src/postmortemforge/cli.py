ac0841e64beaceff0a26dd1b9a626b408b6425c4
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-04-25 11:03:00 +0100

    chore: bump ops

--- src/postmortemforge/cli.py
+++ src/postmortemforge/cli.py
@@ -132,3 +132,14 @@ def _cmd_draft(args) -> int:
     tl = build(aligned, window_s=args.window)
     draft = build_draft(tl)
     sys.stdout.write(draft.render())
+    return FINDINGS if tl.links else CLEAN
+
+
+def _cmd_version(args) -> int:
+    sys.stdout.write(f"postmortemforge {__version__}\n")
+    return CLEAN
+
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="postmortemforge",
