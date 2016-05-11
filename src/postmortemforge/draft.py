637aef8f146502f19cabbc56e9c16b2851c7fd89
francescobrouwer <309159529+francescobrouwer@users.noreply.github.com>
2016-05-11 18:33:00 +0100

    refactor: ci

--- src/postmortemforge/draft.py
+++ src/postmortemforge/draft.py
@@ -16,3 +16,14 @@ from dataclasses import dataclass
 
 from .sources import Provenance
 from .timeline import Timeline, iso_utc
+
+
+class UngroundedStatement(ValueError):
+    """Raised if code attempts to build a Claim with no source span."""
+
+
+@dataclass(frozen=True)
+class Claim:
+    """A single postmortem statement and the source spans that ground it."""
+
+    text: str
