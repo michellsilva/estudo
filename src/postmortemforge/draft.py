3e85a2af3c5355381b34f75aa7e45ba0b7f200c8
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-05-16 10:42:00 +0100

    chore: bump drafting

--- src/postmortemforge/draft.py
+++ src/postmortemforge/draft.py
@@ -48,3 +48,14 @@ class Draft:
     def render(self) -> str:
         lines: list[str] = [f"# {self.title}", ""]
         for name, claims in self.sections:
+            lines.append(f"## {name}")
+            if not claims:
+                lines.append("(no statement could be grounded in a source span)")
+            for claim in claims:
+                lines.append(claim.render())
+            lines.append("")
+        return "\n".join(lines).rstrip() + "\n"
+
+
+def _prov(*aligned_events) -> tuple[Provenance, ...]:
+    return tuple(ae.event.prov for ae in aligned_events)
