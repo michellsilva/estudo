b68cd6c3b106bbe5b07f79cfde06dba2f37a0ff8
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-05-22 14:52:00 +0100

    refactor: tidy reports

--- src/postmortemforge/draft.py
+++ src/postmortemforge/draft.py
@@ -91,3 +91,14 @@ def build_draft(timeline: Timeline) -> Draft:
         ref = ae.event.attrs.get("ref", "")
         mins = timeline.minutes(ae.ref_ts)
         if action == "deploy":
+            summary.append(
+                Claim(
+                    text=f"A deploy of {ref} occurred at T+{mins:0.1f} min.",
+                    sources=_prov(ae),
+                )
+            )
+        elif action == "rollback":
+            summary.append(
+                Claim(
+                    text=f"A rollback to {ref} occurred at T+{mins:0.1f} min.",
+                    sources=_prov(ae),
