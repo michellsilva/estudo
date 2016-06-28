eb8fd5cd8299432d542fd3a0750ed9aa317ad17c
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-06-28 17:24:00 +0100

    test: cover ops

--- src/postmortemforge/report.py
+++ src/postmortemforge/report.py
@@ -111,3 +111,15 @@ def render_svg(timeline: Timeline) -> str:
         'minute axis measured from the first event. Markers show each event, and '
         'connectors show correlated deploy, breach, and rollback relationships.</desc>'
     )
+    parts.append(f'<rect x="0" y="0" width="{width:g}" height="{height:g}" fill="{_WHITE}"/>')
+
+    # Title text.
+    parts.append(
+        f'<text x="{left:g}" y="36" font-family="-apple-system, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif" '
+        f'font-size="17" font-weight="600" fill="{_INK}">Aligned incident timeline</text>'
+    )
+    parts.append(
+        f'<text x="{left:g}" y="56" font-family="-apple-system, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif" '
+        f'font-size="12" fill="{_MUTED}">minutes from first event, reference clock</text>'
+    )
+
