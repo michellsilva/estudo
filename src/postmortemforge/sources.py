1f5a39303fec3d5de0b6935028eccfdc7c6e5ad6
camillejackson <309327822+camillejackson@users.noreply.github.com>
2016-08-06 13:15:00 +0100

    feat: evidence

--- src/postmortemforge/sources.py
+++ src/postmortemforge/sources.py
@@ -148,3 +148,16 @@ def read_metric(text: str, path: str) -> tuple[MetricMeta, list[Event]]:
             continue
         if s.startswith("#"):
             if s.startswith("# metric"):
+                meta = _parse_metric_header(s, path, line_no)
+            continue
+        if meta is None:
+            raise SourceError(f"{path}:{line_no}: metric data before header")
+        parts = s.split()
+        if len(parts) != 2:
+            raise SourceError(f"{path}:{line_no}: metric line needs a timestamp and value")
+        ts = _parse_ts(parts[0], path, line_no)
+        try:
+            value = float(parts[1])
+        except ValueError as exc:
+            raise SourceError(f"{path}:{line_no}: bad metric value {parts[1]!r}") from exc
+        breached = meta.breached(value)
