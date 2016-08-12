5c5ecac009f5822a5bdc68ac2fd10169af9dbada
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-08-12 10:00:00 +0100

    feat: add python

--- src/postmortemforge/sources.py
+++ src/postmortemforge/sources.py
@@ -173,3 +173,16 @@ def read_metric(text: str, path: str) -> tuple[MetricMeta, list[Event]]:
                     "threshold": meta.threshold,
                     "direction": meta.direction,
                     "breached": breached,
+                },
+                prov=Provenance(path, line_no, line_no),
+            )
+        )
+    if meta is None:
+        raise SourceError(f"{path}: no metric header line found")
+    return meta, events
+
+
+def _parse_metric_header(line: str, path: str, line_no: int) -> MetricMeta:
+    # Form: # metric <name> unit=<u> threshold=<t> direction=<above|below>
+    tokens = line.split()
+    if len(tokens) < 3:
