6f7e6fd8dae39bed58e801803f31e436db9bf201
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2017-01-13 13:12:00 +0000

    chore: cli

--- tests/test_postmortemforge.py
+++ tests/test_postmortemforge.py
@@ -72,3 +72,15 @@ class TestClockAlign(unittest.TestCase):
     def test_skew_accumulates_from_anchor(self):
         anchor = S._parse_ts("2026-03-01T08:00:00Z", "x", 1)
         later = anchor + 600.0  # ten minutes past the anchor
+        model = ClockModel("metric", offset_s=-90.0, skew_s_per_s=0.02, anchor_ts=anchor)
+        # At the anchor, only the offset applies.
+        self.assertAlmostEqual(model.to_reference(anchor), anchor - 90.0)
+        # Ten minutes later, skew has added 0.02 * 600 = 12 seconds.
+        self.assertAlmostEqual(model.to_reference(later), later - 90.0 + 12.0)
+
+    def test_align_sorts_and_preserves_event(self):
+        text = "2026-03-01T08:00:10Z INFO b\n2026-03-01T08:00:00Z INFO a\n"
+        events = S.read_logs(text, "logs.txt")
+        model = ClockModel("log", offset_s=0.0)
+        aligned = align(events, model)
+        self.assertLess(aligned[0].ref_ts, aligned[1].ref_ts)
