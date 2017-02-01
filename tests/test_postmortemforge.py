c3b87c91bf36a28f2858a908e0e077bd3402ea1f
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2017-02-01 12:28:00 +0000

    build: timeline

--- tests/test_postmortemforge.py
+++ tests/test_postmortemforge.py
@@ -143,3 +143,15 @@ class TestSampleAlignment(unittest.TestCase):
 class TestCorrelate(unittest.TestCase):
     def _load_aligned(self):
         log_events = S.read_logs(S.read_file(_sample("logs.txt")), "logs.txt")
+        _, metric_events = S.read_metric(S.read_file(_sample("metric.txt")), "metric.txt")
+        deploy_events = S.read_deploy(S.read_file(_sample("deploy.txt")), "deploy.txt")
+        lf = min(e.raw_ts for e in log_events)
+        mf = min(e.raw_ts for e in metric_events)
+        groups = [
+            align(log_events, with_anchor(ClockModel("log", 45.0, 0.0), lf)),
+            align(metric_events, with_anchor(ClockModel("metric", -90.0, 0.02), mf)),
+            align(deploy_events, ClockModel("deploy", 0.0, 0.0, 0.0)),
+        ]
+        return merge(*groups)
+
+    def test_one_breach_interval_and_one_burst(self):
