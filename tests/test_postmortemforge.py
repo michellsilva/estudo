e567dfb69067c7bd6a82e49d50717b19112b31fa
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2017-03-01 09:05:00 +0000

    test: cover drafting

--- tests/test_postmortemforge.py
+++ tests/test_postmortemforge.py
@@ -202,3 +202,15 @@ class TestDraft(unittest.TestCase):
         tl = build(aligned)
         draft = build_draft(tl)
         rendered = draft.render()
+        for line in rendered.splitlines():
+            if line.startswith("- "):
+                self.assertIn("[", line)
+                self.assertIn("]", line)
+                self.assertRegex(line, r"\[[^\]]*:\d")
+
+    def test_draft_is_byte_identical_across_runs(self):
+        log_events = S.read_logs(S.read_file(_sample("logs.txt")), "logs.txt")
+        _, metric_events = S.read_metric(S.read_file(_sample("metric.txt")), "metric.txt")
+        deploy_events = S.read_deploy(S.read_file(_sample("deploy.txt")), "deploy.txt")
+        lf = min(e.raw_ts for e in log_events)
+        mf = min(e.raw_ts for e in metric_events)
